bd3ce425f8c1a2a6c64141c9f54db468d1fbf293
xiaosean <aa26365566@gmail.com>
2020-11-21 12:55:53 +0800

    Feat: Apply spade normalization

--- models/networks.py
+++ models/networks.py
@@ -993,11 +993,18 @@ class ResnetGenerator(nn.Module):
                           nn.ReLU(True)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        self.norm_spade = SPADE(output_nc, output_nc, param_free_norm_type="instance")
+        model += [self.norm_spade]
         model += [nn.Tanh()]
+        print("self.norm_spade")
+                
+
 
         self.model = nn.Sequential(*model)
 
     def forward(self, input, layers=[], encode_only=False):
+        # 33
+        # print("output length =", len(self.model)) 
         if -1 in layers:
             layers.append(len(self.model))
         if len(layers) > 0:
@@ -1595,5 +1602,67 @@ class Self_Attn_Coord(nn.Module):
 
         if self.use_input:
             out += x
+
+        return out
+
+
+# Creates SPADE normalization layer based on the given configuration
+# SPADE consists of two steps. First, it normalizes the activations using
+# your favorite normalization method, such as Batch Norm or Instance Norm.
+# Second, it applies scale and bias to the normalized output, conditioned on
+# the segmentation map.
+# The format of |config_text| is spade(norm)(ks), where
+# (norm) specifies the type of parameter-free normalization.
+#       (e.g. syncbatch, batch, instance)
+# (ks) specifies the size of kernel in the SPADE module (e.g. 3x3)
+# Example |config_text| will be spadesyncbatch3x3, or spadeinstance5x5.
+# Also, the other arguments are
+# |norm_nc|: the #channels of the normalized activations, hence the output dim of SPADE
+# |label_nc|: the #channels of the input semantic map, hence the input dim of SPADE
+# Code borrowed from
+class SPADE(nn.Module):
+    def __init__(self, norm_nc, label_nc, param_free_norm_type='instance'):
+        super().__init__()
+
+
+        ks = 3
+
+        if param_free_norm_type == 'instance':
+            self.param_free_norm = nn.InstanceNorm2d(norm_nc, affine=False)
+        elif param_free_norm_type == 'batch':
+            self.param_free_norm = nn.BatchNorm2d(norm_nc, affine=False)
+        else:
+            raise ValueError('%s is not a recognized param-free norm type in SPADE'
+                             % param_free_norm_type)
+
+        # The dimension of the intermediate embedding space. Yes, hardcoded.
+        nhidden = 128
+
+        pw = ks // 2
+        self.mlp_shared = nn.Sequential(
+            nn.Conv2d(label_nc, nhidden, kernel_size=ks, padding=pw),
+            nn.ReLU()
+        )
+        self.mlp_gamma = nn.Conv2d(nhidden, norm_nc, kernel_size=ks, padding=pw)
+        self.mlp_beta = nn.Conv2d(nhidden, norm_nc, kernel_size=ks, padding=pw)
+
+    def forward(self, x, segmap=None):
+
+            
+        # Part 1. generate parameter-free normalized activations
+        normalized = self.param_free_norm(x)
+
+        if segmap is None:
+            segmap = x
+        else:
+            # Part 2. produce scaling and bias conditioned on semantic map
+            segmap = F.interpolate(segmap, size=x.size()[2:], mode='nearest')
+        
+        actv = self.mlp_shared(segmap)
+        gamma = self.mlp_gamma(actv)
+        beta = self.mlp_beta(actv)
+
+        # apply scale and bias
+        out = normalized * (1 + gamma) + beta
 
         return out
