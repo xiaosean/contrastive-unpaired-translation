49d8284cbc78c9e2e8746bcfb11b661b7545321e
xiaosean <aa26365566@gmail.com>
2020-12-01 17:09:38 +0800

    Implement data augmentation

--- options/train_options.py
+++ options/train_options.py
@@ -19,6 +19,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--update_html_freq', type=int, default=1000, help='frequency of saving training results to html')
         parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
         parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
+        parser.add_argument('--use_data_augmentation', action='store_true', help='use data augmentation while training to fit selfie2anime')
         # network saving and loading parameters
         parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
